ffa3e836531b82e671977a1d86c365235adffa4d
CesarIgnacio <cesarignac@gmail.com>
2022-09-08 02:54:40 -0400

    Update ex1-5.cpp

--- Exercise2/ex1-5.cpp
+++ Exercise2/ex1-5.cpp
@@ -10,7 +10,7 @@ int main() {
 
     std::cout << "The sum of " << v1;
     std::cout << " and " << v2;
-    std::cout << "i s " << v1 + v2 << "\n";
+    std::cout << "is " << v1 + v2 << "\n";
 
     return 0;
 }
