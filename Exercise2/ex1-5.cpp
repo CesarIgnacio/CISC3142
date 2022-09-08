1fbcc2d124fb92d34f2854a743b53c5a38be6a34
CesarIgnacio <cesarignac@gmail.com>
2022-09-08 02:47:22 -0400

    Create ex1-5

---
+++ Exercise2/ex1-5.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main() {
+
+    int v1, v2;
+
+    std::cout << "Inser two integer numbers\n";
+    std::cin >> v1;
+    std::cin >> v2;
+
+    std::cout << "The sum of " << v1;
+    std::cout << " and " << v2;
+    std::cout << "i s " << v1 + v2 << "\n";
+
+    return 0;
+}
