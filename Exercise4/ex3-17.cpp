051341f4a99823bdb2ae030676eccc8ef745debf
root <root@DESKTOP-E8ASCDM.localdomain>
2022-09-29 10:44:49 -0400

    adding ex3-17, ex3-23 and ex4-28, with their executables in c++

--- Exercise4/ex3-17.cpp
+++ Exercise4/ex3-17.cpp
@@ -1,7 +1,39 @@
 #include <iostream>
-#include <string>
-#include <vertor>
+#include <vector>
+using namespace std;
 
-int main() {
-    return 0;
+int main()
 {
+
+    vector<string> myVec;
+    int count8 = 0;
+
+    string str0;
+    cout << "please enter a string. 'end' stops the program" << endl;
+    cin >> str0;
+
+    while (str0 != "end") {
+         
+        for (char &c : str0)
+            c = toupper(c);
+
+        myVec.push_back(str0);
+        cout << "enter another string ";
+        cin >> str0;
+    }
+
+    cout << "done inputing";
+
+    cout << "Printing Vector elements: \n";
+
+    for(string e : myVec) {
+        count8++;
+        cout << e << " ";
+        
+        if(count8 % 8 == 0) {
+            cout << endl;
+        }
+    }
+    cout << endl;
+    return 0;
+}
